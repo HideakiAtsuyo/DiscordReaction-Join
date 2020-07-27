48f3c538615eafec33df14bfbb2122bf73457d60
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-07-27 09:05:02 +0200

    Fuck

---
+++ DiscordReactions/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Leaf.xNet;
+namespace DiscordReactions
+{
+    class Program
+    {
+        public static List<string> Tokens = new List<string>();
+        public static int JoinCount = 0;
+
+        public static bool Join(string invite, string token)
+        {
+            CookieContainer cookieContainer = new CookieContainer();
+
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create("https://discord.gg/" + invite);
+                request.Headers["authorization"] = token;
+                request.CookieContainer = cookieContainer;
+                var response = (HttpWebResponse)request.GetResponse();
+                string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+
+                var request3 = (HttpWebRequest)WebRequest.Create("https://discord.com/api/v6/invites/" + invite);
+                request3.Headers["authorization"] = token;
+                request3.Method = "POST";
+                request3.ContentType = "application/json";
+                request3.CookieContainer = cookieContainer;
+                request3.ContentLength = 0;
+
+                var r3 = (HttpWebResponse)request3.GetResponse();
+                var rs3 = new StreamReader(r3.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(rs3);
+                return true;
+            }
+            catch (WebException ex)
+            {
+                string response = new StreamReader(ex.Response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(response);
+                return false;
+            }
+
+        }
+
+        public static bool React(string CHID, string MSGID, string emoji, string token)
+        {
+            try
+            {
+                emoji = Uri.EscapeUriString(emoji);
+                var request3 = (HttpWebRequest)WebRequest.Create("https://discord.com/api/v6/channels/" + CHID + "/messages/" + MSGID + "/reactions/" + emoji + "/%40me");
+
+                request3.Headers["authorization"] = token;
+                request3.Method = "PUT";
+                request3.ContentType = "application/json";
+                request3.ContentLength = 0;
+                var r3 = (HttpWebResponse)request3.GetResponse();
+                var rs3 = new StreamReader(r3.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(rs3);
+                return true;
+            }
+            catch (WebException ex)
+            {
+                string response = new StreamReader(ex.Response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(response);
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = $"Discord Joiner & Reacter";
+            Console.WriteLine("Tokens file path(only name.txt if he is in the folder): ");
+            var tokensfile = Console.ReadLine();
+            foreach (string token in File.ReadLines(tokensfile, Encoding.UTF8)){Tokens.Add(token);}
+            Console.Title = $"Discord Joiner & Reacter | Tokens: {Tokens.Count()}";
+            /////////////////Lol/////////////////
+            Console.WriteLine("Invite: ");
+            var invite = Console.ReadLine();
+            Console.WriteLine("Channel ID: ");
+            var chanid = Console.ReadLine();
+            Console.WriteLine("Message ID: ");
+            var messageid = Console.ReadLine();
+            Console.WriteLine("React: ");
+            var Reaction = Console.ReadLine();
+            /////////////////////////////////////
+            foreach (string token in Tokens)
+            {
+                new Thread(delegate ()
+                {
+                    Join(invite, token);
+                    Thread.Sleep(120);
+                    React(chanid, messageid, Reaction, token);
+                }).Start();
+                Thread.Sleep(50);
+            }
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DiscordReactions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("DiscordReactions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiscordReactions")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("7e9d6c16-0609-4042-81da-ce4f48d7cfa3")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
